c855a4d41bec15d09f34d788368485dc9ded0bd2
DiantarPermata <167069623+DiantarPermata@users.noreply.github.com>
2024-05-20 16:15:43 +0700

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,41 @@
-import 'package:dashboard/pages/pagescreen.dart';
+import 'package:Dashboard/pages/dashboard/dashboard.dart';
+import 'package:Dashboard/pages/documents/documents.dart';
+import 'package:Dashboard/pages/notif/notif.dart';
+import 'package:Dashboard/pages/pagescreen.dart';
+import 'package:Dashboard/pages/profile/profile.dart';
+import 'package:Dashboard/pages/settings/settings.dart';
+import 'package:Dashboard/pages/store/store.dart';
+import 'package:Dashboard/pages/task/task.dart';
+import 'package:Dashboard/pages/transaction/transaction.dart';
+import 'package:Dashboard/theme/color.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Dashboard',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primaryColor: background,
         useMaterial3: true,
       ),
       home: PageScreen(),
+      routes: {
+        '/dashboard': (context) => Dashboard(),
+        '/transaction': (context) => Transaction(),
+        '/task': (context) => Task(),
+        '/documents': (context) => Documents(),
+        '/store': (context) => Store(),
+        '/notification': (context) => Notif(),
+        '/profile': (context) => Profile(),
+        '/settings': (context) => Settings(),
+      },
     );
   }
 }
